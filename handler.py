b71db0f299dcc515488742c2b079fa63ec68eace
Manuel Maldonado <19945996+Glitchr@users.noreply.github.com>
2024-01-04 00:29:21 -0400

    Implement retry mechanism for handling rate limiting errors when sending messages through telegram

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
-import requests
+import requests, time
 from decouple import config
 import log
 
@@ -11,23 +11,29 @@ url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
 
 def send_chapter(chapter):
     """Sends the new chapter information to the telegram chat"""
-    try:
-        # Send a POST request with the parameters
-        response = requests.post(url, data={
-                                    'chat_id': TELEGRAM_CHAT_ID,
-                                    'text': chapter})
-        # Check the status code and the response content
-        if response.status_code == 200:
-            # Log a success message
-            logger.info(f"Successfully sent message: {chapter}")
-            print("Message sent successfully!")
-        else:
-            # Log an error message with the status code
-            logger.error(f"Failed to send message: {chapter}", exc_info=True)
-            print("Something went wrong!")
-            print(response.text)
+    max_retries = 5
+    retry_delay = 60
 
-    # Handle any requests exceptions
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Failed to send message: {chapter}", exc_info=True)
-        raise e
+    for attempt in range(max_retries):
+        try:
+            # Send a POST request with the parameters
+            response = requests.post(url, data={
+                                        'chat_id': TELEGRAM_CHAT_ID,
+                                        'text': chapter})
+            # Check the status code and the response content
+            if response.status_code == 200:
+                # Log a success message
+                logger.info(f"Successfully sent message: {chapter}")
+                print("Message sent successfully!")
+                break
+            else:
+                # Log an error message with the status code
+                logger.error(f"Failed to send message: \n{response.text}\n{chapter}", exc_info=True)
+                print("Something went wrong!")
+                print(response.text)
+
+        # Handle any requests exceptions
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Failed to send message: {chapter}", exc_info=True)
+            print(f"Attempt {attempt+1} failed, retrying in {retry_delay} seconds...")
+            time.sleep(retry_delay)  # Wait before retrying
